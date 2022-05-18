0086ea6e61f042c2f3c7a449a169767f46c34e84
Victoria Kee <victoriakee95@gmail.com>
2022-05-18 15:29:19 -0500

    set up todo list and stylings

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField'
+import TodoList from './components/TodoList';
 import Todo from "./model";
 
 const App: React.FC = () => {
@@ -23,9 +24,7 @@ const App: React.FC = () => {
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-      {todos.map((t) => (
-        <li>{t.todo}</li>
-      ))}
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
