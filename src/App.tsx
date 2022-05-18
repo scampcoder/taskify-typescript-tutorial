668ee29bf5421c5842f230ba6abd4d119c966634
Victoria Kee <victoriakee95@gmail.com>
2022-05-18 12:02:29 -0500

    set up input field with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
+import InputField from './components/InputField'
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("")
+
+  console.log(todo)
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
