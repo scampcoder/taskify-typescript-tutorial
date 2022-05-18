a29cb85f298cd59b4a8598f57fd27d287f0db38a
Victoria Kee <victoriakee95@gmail.com>
2022-05-18 15:40:46 -0500

    implt marked done functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -11,17 +11,31 @@ type Props = {
 }
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+
+    const handleDone = (id: number) => {
+        setTodos(todos.map((todo) => 
+            todo.id === id ? {...todo, isDone: !todo.isDone} : todo
+        ))
+    }
+    
     return (
         <form className="todos__single">
-            <span className="todos__single--text">{todo.todo}</span>
+            {
+                todo.isDone ? (
+                    <s className="todos__single--text">{todo.todo}</s>
+                ) : (
+                    <span className="todos__single--text">{todo.todo}</span>
+                )
+            }
+            
             <div>
                 <span className="icon">
                     <AiFillEdit />
                 </span>
-                <span className="icon">
+                <span className="icon" >
                     <AiFillDelete />
                 </span>
-                <span className="icon">
+                <span className="icon" onClick={() => handleDone(todo.id)}>
                     <MdDone/>
                 </span>
             </div>
